1ff3d906d237838865dc3a4af1144c15e91e9912
NicolasNPP <Nicolasnahuel94@gmail.com>
2022-04-11 15:35:55 -0300

    AUTH JWT.

--- Daos/Usuarios/usuariosDaoMongo.js
+++ Daos/Usuarios/usuariosDaoMongo.js
@@ -6,9 +6,9 @@ class usuariosDaoMongo extends moduloMon.Contenedor {
 
     constructor() {
         const s = new mongoose.Schema({
+            _id: { type: String, required: false },
             username: { type: String, required: true },
-            password: { type: String, required: true }
-
+            password: { type: String, required: true },
         });
         super('usuarios', s, mongoose.model('usuarios', s))
     }
